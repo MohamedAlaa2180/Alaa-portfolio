adcbdf46a64e84573bb14997065e795f4e636cea
Mohamed Alaa <mohamedalaasalem1@gmail.com>
2025-11-21 04:31:22 +0200

    feat: add Cascadia Code font for monospace elements

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,7 @@ export default {
       },
       fontFamily: {
         sans: ['Inter', 'system-ui', 'sans-serif'],
+        mono: ['Cascadia Code', 'Consolas', 'Monaco', 'monospace'],
       },
     },
   },
